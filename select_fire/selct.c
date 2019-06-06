014f35ea47ef9a48a2a7f7b1132c9af5647a728d
jiejietop <1161959934@qq.com>
2019-06-06 23:07:12 +0800

    update selct.c

--- select_fire/selct.c
+++ select_fire/selct.c
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 
 
-/**函数原型:
+/**
+ * 函数原型:
  * int select(int numfds, fd_set *readfds, fd_set *writefds，
  *            fd_set *exeptfds, struct timeval *timeout) 
  * numfds：该参数值为需要监视的文件描述符的最大值加 1
@@ -19,6 +20,19 @@
  * return : 大于 0：成功，返回准备好的文件描述符的数目
  *          0：超时
  *          -1：出错
+ */
+
+/********************* select()文件描述符处理函数 *****************************
+ * 
+ * FD_ZERO(fd_set *set)             清除一个文件描述符集
+ * FD_SET(int fd, fd_set *set)      将一个文件描述符加入文件描述符集中
+ * FD_CLR(int fd, fd_set *set)      将一个文件描述符从文件描述符集中清除
+ * FD_ISSET(int fd, fd_set *set)    如果文件描述符 fd 为 fd_set 集中的一个元素，则返回非零值，
+ * 可以用于调用 select()之后测试文件描述符集中的文件描述符是否有变化 
+ * 
+ * 一般来说，在使用 select()函数之前，首先使用 FD_ZERO()和 FD_SET()来初始化文件描述符集，在使用了
+ * select()函数时，可循环使用 FD_ISSET()来测试描述符集，在执行完对相关文件描述符的操作之后，使用
+ * FD_CLR()来清除描述符集。 
  */
 
 int main(void)
