197f5fa0d39c71134477aaad324c930b873538c8
jiejietop <1161959934@qq.com>
2019-06-06 23:33:22 +0800

    update gitignore opt

--- select_fire/selct.c
+++ select_fire/selct.c
@@ -1,14 +1,9 @@
-#include <sys/types.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 
 /**
  * 函数原型:
  * int select(int numfds, fd_set *readfds, fd_set *writefds，
  *            fd_set *exeptfds, struct timeval *timeout) 
+ * 
  * numfds：该参数值为需要监视的文件描述符的最大值加 1
  * readfds：由 select()监视的读文件描述符集合
  * writefds：由 select()监视的写文件描述符集合
@@ -35,11 +30,122 @@
  * FD_CLR()来清除描述符集。 
  */
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_BUFFER_SIZE 1024 /* 缓冲区大小*/
+#define IN_FILES 3 /* 多路复用输入文件数目*/
+#define TIME_DELAY 60 /* 超时值秒数 */
+#define MAX(a, b) ((a > b)?(a):(b))
+
+
 int main(void)
 {
+    int fds[IN_FILES];
+    char buf[MAX_BUFFER_SIZE];
+    int i, res, real_read, maxfd;
 
+    struct timeval tv;
+    fd_set inset,tmp_inset;
 
+    /*首先以只读非阻塞方式打开两个管道文件*/
+    fds[0] = 0;
+    if((fds[1] = open ("in1", O_RDONLY|O_NONBLOCK)) < 0)
+    {
+        printf("Open in1 error\n");
+        return 1;
+    }
 
+    if((fds[2] = open ("in2", O_RDONLY|O_NONBLOCK)) < 0)
+    {
+        printf("Open in2 error\n");
+        return 1;
+    }
+
+    /*取出两个文件描述符中的较大者*/
+    maxfd = MAX(MAX(fds[0], fds[1]), fds[2]);
+    /*初始化读集合 inset，并在读集合中加入相应的描述集*/
+    FD_ZERO(&inset);
+    for (i = 0; i < IN_FILES; i++)
+    {
+        FD_SET(fds[i], &inset);
+    }
+
+    FD_SET(0, &inset);
+    tv.tv_sec = TIME_DELAY;
+    tv.tv_usec = 0;
+    /*循环测试该文件描述符是否准备就绪，并调用 select 函数对相关文件描述符做对应操作*/
+    while(FD_ISSET(fds[0],&inset)|| FD_ISSET(fds[1],&inset) || FD_ISSET(fds[2], &inset))
+    {
+        /* 文件描述符集合的备份， 这样可以避免每次进行初始化 */
+        tmp_inset = inset;
+        res = select(maxfd + 1, &tmp_inset, NULL, NULL, &tv);
+        switch(res)
+        {
+            case -1:
+            {
+                printf("Select error\n");
+                return 1;
+            }
+            break;
+
+            case 0: /* Timeout */
+            {
+                printf("Time out\n");
+                return 1;
+            }
+            break;
+
+            default:
+            {
+                for (i = 0; i < IN_FILES; i++)
+                {
+                    if(FD_ISSET(fds[i], &tmp_inset))
+                    {
+                        memset(buf, 0, MAX_BUFFER_SIZE);
+                        real_read = read(fds[i], buf, MAX_BUFFER_SIZE);
+                        
+                        if (real_read < 0)
+                        {
+                            if (errno != EAGAIN)
+                            {
+                                return 1;
+                            }
+                        }
+
+                        else if (!real_read)
+                        {
+                            close(fds[i]);
+                            FD_CLR(fds[i], &inset);
+                        }
+                        else
+                        {
+                            if (i == 0)
+                            {/* 主程序终端控制 */
+                                if ((buf[0] == 'q') || (buf[0] == 'Q'))
+                                {
+                                    return 1;
+                                }
+                            }
+                        else
+                        {
+                            /* 显示管道输入字符串 */
+                            buf[real_read] = '\0';
+                            printf("%s", buf);
+                        }
+                        }
+                    } /* end of if */
+                } /* end of for */
+            }
+            break;
+            
+        } /* end of switch */
+    } /*end of while */
     return 0;
 }
 
